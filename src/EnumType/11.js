528df408ef4d1b88aa961f8452839d9b6f5d9ac9
jw.kim <babamba88@gintlab.com>
2020-10-05 14:00:08 +0900

    feat: :sparkles: EnumType 마저 스터디

---
+++ src/EnumType/11.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var oper = { cycle: '11', date_used: 11 };
+console.log('oper : ', oper);
+// const enum 을 이용해서 컴파일 결과에 enum 객체를 남기지 않을 수 있다.
+// 하지만 컴파일 결과물에 없기때문에 위의 함수를 사용할 수 없다. 타입에러
+//getIsValidEnumValue(Fruit, 1);
---
+++ src/EnumType/11.ts
@@ -0,0 +1,13 @@
+export {};
+
+interface OperMonth {
+  cycle: string;
+  date_used: number;
+}
+
+const oper: OperMonth = { cycle: '11', date_used: 11 };
+console.log('oper : ', oper);
+// const enum 을 이용해서 컴파일 결과에 enum 객체를 남기지 않을 수 있다.
+
+// 하지만 컴파일 결과물에 없기때문에 위의 함수를 사용할 수 없다. 타입에러
+//getIsValidEnumValue(Fruit, 1);
